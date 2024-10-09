55952dca2e2fc2768861edea2eae2baee78df878
Soham-1827 <sohamchoulwar.27@gmail.com>
2024-10-08 23:03:12 -0700

    Added reset ka working and time functionality and otp functionality

--- FirstDatabaseMaven/src/main/java/edu/asu/DatabasePart1/Admin.java
+++ FirstDatabaseMaven/src/main/java/edu/asu/DatabasePart1/Admin.java
@@ -1,9 +1,10 @@
 package edu.asu.DatabasePart1;
 import java.util.*;
-
+import java.sql.Time;
+import java.time.*;
 
 public class Admin{
-	private String username;
+	private String username; 
 	static Scanner scan = new Scanner(System.in);
 	static char[] generateOTP() {
 		char[] otp = new char[5]; // Array to hold the characters
@@ -18,20 +19,42 @@ public class Admin{
         return otp;
 	}
 	
-	static void reset(String username) {
+	static void reset(String email) {
 		char[] otp = generateOTP();
 		int cond = 0;
+		
 		String otp_gen = "";
+		LocalTime otp_time =LocalTime.now();
+		int minutes = otp_time.getMinute();
+		
 		for(int i = 0; i < 5; i++) {
 			otp_gen = otp_gen + Character.toString(otp[i]);
 			}
+		
 		System.out.println("Enter OTP: ");
 		String otp_user = scan.nextLine();
-		
+//		String str_otp_time= otp_time.toString();
+//		String str_otp_exp= otp_expiration.toString();
+//		
 		if(otp_user.equals(otp_gen)) {
-			System.out.println("Enter the new password: ");
-			String new_pass = scan.nextLine();
-		}
+			LocalTime curr_time=LocalTime.now();
+			int curr_min=curr_time.getMinute();
+			if(curr_min-minutes<=5) {
+				System.out.println("Enter the new password: ");
+				String new_pass = scan.nextLine();
+				
+				while(cond == 0) {
+					if(PasswordEvaluator.evaluatePassword(new_pass) == "Success") {
+						//DatabaseHelper.resetPassword(email, new_pass);
+						cond = 1;
+					}
+					else {
+						cond = 0;
+					}
+				}
+			}
+			
+		
 		
 		}
 		
